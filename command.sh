126ff3a23ea8add435b2271efa2cb6936d2442ef
Gentleelephant <birdhk@kubesphere.io>
2022-11-15 10:33:10 +0800

    Edit command

--- command.sh
+++ command.sh
@@ -2,6 +2,14 @@
 
 # 在test server执行命令
 
+hostname=`hostname`
 
+git pull https://github.com/Gentleelephant/v2config.git
+
+# restart v2ray
+service v2ray restart
+
+date >> /var/log/v2ray/command.log
+echo "restart v2ray" >> /var/log/v2ray/command.log
 
 # 在hk server执行命令
